29d8386cc33ada976c279402b4bfb0d6370be683
CamelGamer123 <kylepannan@gmail.com>
2023-02-21 14:49:27 +1000

    Update codegen.py

--- code_generator/codegen.py
+++ code_generator/codegen.py
@@ -1,13 +1,70 @@
-import csv
-import string
-import threading
-import time
-import sys
-
-
-class generator(object):
+class Generator(object):
     def __init__(self):
         self.description = """This is the object that generates the codes, do not modify"""
+        # Declare Dictionary
+        self.list = {}
+
+    def generate(self, length, list_of_strings, minimum_length=1, output_mode="List", file_out=False):
+        """
+        Main function.
+
+        :parameter file_out:  If True, will output to a file.  If False, will output to console.
+        :parameter length:  The maximum length of combinations to be generated.
+        :parameter minimum_length:  The minimum length of combinations to be generated.
+        :parameter list_of_strings:  A list of strings to be used to generate combinations.
+        :parameter output_mode:  The output mode.  Can be "List" or "Set". Set is faster but is unordered.
+        :return:
+        """
+
+        try:
+            inputDigits = int(length)
+        except ValueError:
+            raise ValueError("Length must be an integer")
+
+        try:
+            minimumDigits = int(minimum_length)
+        except ValueError:
+            raise ValueError("Minimum Length must be an integer")
+
+        totalDigits = inputDigits + 1
+
+        listNameLoopingDigits = inputDigits
+
+        # Test Define Lists Loop
+        for i in range(inputDigits):
+            listNameIncStr = "list" + str(listNameLoopingDigits)
+            print(listNameIncStr)
+            listNameLoopingDigits -= 1
+
+        # Create All Lists
+        self.createAllLists(totalDigits, minimumDigits)
+
+        rollingdigits = inputDigits
+        for i in range(inputDigits):
+            if rollingdigits <= inputDigits:
+                print("Lists Created In Same Directory As Program")
+                exit("Program completed")
+            else:
+                print(rollingdigits, "Digits")
+                rollingdigits -= 1
+                rollingK = rollingdigits
+                filename = "file" + str(rollingdigits)
+                listName = "list" + str(rollingdigits)
+
+                printAllKLength(list_of_strings, rollingK, filename, list[listName])
+
+                print(list[listName])
+
+                fileToPrint = open("f" + str(rollingdigits) + ".txt", "w")
+                for element in self.list[listName]:
+                    fileToPrint.write(element + "\n")
+                fileToPrint.close()
+
+    def createAllLists(self, highestDigit, lowestDigit):
+        for i in range(highestDigit):
+            if i >= lowestDigit:
+                listName = "list" + str(i)
+                self.list[listName] = []
 
 
 def printAllKLength(set, k, targetFile, listNumber):
@@ -36,104 +93,3 @@ def printAllKLengthRec(set, prefix, n, k, targFile, listName):
         # k is decreased, because
         # we have added a new character
         printAllKLengthRec(set, newPrefix, n, k - 1, targFile, listName)
-
-
-def logic(currentDigit):
-    rollingK = currentDigit
-    filename = "file" + str(currentDigit)
-    listName = "list" + str(currentDigit)
-
-    printAllKLength(set1, rollingK, filename, list[listName])
-
-    print(list[listName])
-
-    fileToPrint = open("f" + str(currentDigit) + ".txt", "w")
-    for element in list[listName]:
-        fileToPrint.write(element + "\n")
-    fileToPrint.close()
-
-
-def printToConsole(digitcount, digitCutoff):
-    rollingdigits = digitcount
-    for i in range(digitcount):
-        if rollingdigits <= digitCutoff:
-            print("Lists Created In Same Directory As Program")
-            sys.exit("Program completed")
-        else:
-            print(rollingdigits, "Digits")
-            rollingdigits -= 1
-            logic(rollingdigits)
-
-
-# Driver Code
-if __name__ == "__main__":
-    # Define Sets
-    # set1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    # set1 = ['A', 'B']
-    set1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
-    inputDigits = 3
-
-    totalDigits = inputDigits + 1
-    minimumDigits = 3
-
-    listNameLoopingDigits = inputDigits
-
-    # Declare Dictionary
-    list = {}
-
-    # Test Define Lists Loop
-    for i in range(inputDigits):
-        listNameIncStr = "list" + str(listNameLoopingDigits)
-        print(listNameIncStr)
-        listNameLoopingDigits -= 1
-
-    # Define List
-    list22 = []
-    list21 = []
-    list20 = []
-    list19 = []
-    list18 = []
-    list17 = []
-    list16 = []
-    list15 = []
-    list14 = []
-    list13 = []
-    list12 = []
-    list11 = []
-    list10 = []
-    list9 = []
-    list8 = []
-    list7 = []
-    list6 = []
-    list5 = []
-    list4 = []
-    list3 = []
-    list2 = []
-    list1 = []
-
-    # Define Dictionary and add list
-    list["list22"] = list22
-    list["list21"] = list21
-    list["list20"] = list20
-    list["list19"] = list19
-    list["list18"] = list18
-    list["list17"] = list17
-    list["list16"] = list16
-    list["list15"] = list15
-    list["list14"] = list14
-    list["list13"] = list13
-    list["list12"] = list12
-    list["list11"] = list11
-    list["list10"] = list10
-    list["list9"] = list9
-    list["list8"] = list8
-    list["list7"] = list7
-    list["list6"] = list6
-    list["list5"] = list5
-    list["list4"] = list4
-    list["list3"] = list3
-    list["list2"] = list2
-    list["list1"] = list1
-
-    printToConsole(totalDigits, minimumDigits)
